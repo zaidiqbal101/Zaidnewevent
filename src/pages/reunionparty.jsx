c5a7bfeaed2856f3806bf777c9a54709b8984b90
Mohd Shahid <mohd47149@gmail.com>
2025-01-04 17:37:38 +0530

    file name changes js to jsx

--- src/pages/reunionparty.jsx
+++ src/pages/reunionparty.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+  import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import Weddingcontact from "./weddingcontact";
