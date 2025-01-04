f2a7f59bb240f7970d953277e5121487c4a79d37
Zaid <zaidiqbaliqbal28@gmail.com>
2025-01-04 17:37:05 +0530

    update 4/1/2025

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,50 @@
 import React from "react";
-import { FaInstagram, FaFacebook, FaYoutube, FaLinkedin } from "react-icons/fa";
-
-const Footer = () => {
-  return (
-    <footer className="bg-[#290F23] text-white py-12 rounded-tl-[90px]">
-      <div className="container mx-auto px-4">
-        {/* Planner Logo and Description */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-          <div className="flex flex-col items-start">
-            <img
-              src="/assets/Aryan-Event-Logo.png"
-              alt="Aryan Logo"
-              className="w-52 h-full mb-4"
-            />
-            <p className="text-left text-lg font-medium max-w-lg">
-              Aryan Events is dedicated to orchestrating your special day in a manner that ensures lasting memories for a lifetime.
-            </p>
-          </div>
-
-          {/* Useful Links and Contact Info */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {/* Useful Links Section */}
-            <div>
-              <h4 className="text-xl font-semibold mb-4">Useful Links</h4>
-              <ul className="space-y-2">
-                <li><a href="/" className="text-gray-400 hover:text-white">Home</a></li>
-                <li><a href="/aboutus" className="text-gray-400 hover:text-white">About</a></li>
-                <li><a href="/gallery" className="text-gray-400 hover:text-white">Gallery</a></li>
-                <li><a href="/contactpage" className="text-gray-400 hover:text-white">Contact Us</a></li>
-              
-              </ul>
-            </div>
-
-            {/* Contact Info Section */}
-            <div>
-              <h4 className="text-xl font-semibold mb-4">Contact Us</h4>
-              <ul className="space-y-2">
-                <li>Kujapi Gaya  <br /> Pin Code-823002</li>
-                <li><a href="mailto:aryaneventsplanner@gmail.com" className="text-gray-400 hover:text-white">aryaneventsplanner@gmail.com</a></li>
-                <li><a href="tel:+91 9220565444" className="text-gray-400 hover:text-white">+91 9220565444</a></li>
-                
-              </ul>
-            </div>
-          </div>
-        </div>
-
+import { FaInstagram, FaFacebook, FaYoutube, FaLinkedin } from "react-icons/fa"; 
+ 
+const Footer = () => { 
+  return ( 
+    <footer className="bg-[#290F23] text-white py-12 rounded-tl-[90px]"> 
+      <div className="container mx-auto px-4"> 
+        {/* Planner Logo and Description */} 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8"> 
+          <div className="flex flex-col items-start"> 
+            <img 
+              src="/assets/Aryan-Event-Logo.png" 
+              alt="Aryan Logo" 
+              className="w-52 h-full mb-4" 
+            /> 
+            <p className="text-left text-lg font-medium max-w-lg"> 
+              Aryan Events is dedicated to orchestrating your special day in a manner that ensures lasting memories for a lifetime. 
+            </p> 
+          </div> 
+ 
+          {/* Useful Links and Contact Info */} 
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8"> 
+            {/* Useful Links Section */} 
+            <div> 
+              <h4 className="text-xl font-semibold mb-4">Useful Links</h4> 
+              <ul className="space-y-2"> 
+                <li><a href="/" className="text-gray-400 hover:text-white">Home</a></li> 
+                <li><a href="/aboutus" className="text-gray-400 hover:text-white">About</a></li> 
+                <li><a href="/gallery" className="text-gray-400 hover:text-white">Gallery</a></li> 
+                <li><a href="/contactpage" className="text-gray-400 hover:text-white">Contact Us</a></li> 
+               
+              </ul> 
+            </div> 
+ 
+            {/* Contact Info Section */} 
+            <div> 
+              <h4 className="text-xl font-semibold mb-4">Contact Us</h4> 
+              <ul className="space-y-2"> 
+                <li>Kujapi Gaya  <br /> Pin Code-823002</li> 
+                <li><a href="mailto:aryaneventsplanner@gmail.com" className="text-gray-400 hover:text-white">aryaneventsplanner@gmail.com</a></li> 
+                <li><a href="tel:+91 9220565444" className="text-gray-400 hover:text-white">+91 9220565444</a></li> 
+                 
+              </ul> 
+            </div> 
+          </div> 
+        </div> 
+ 
         {/* Social Media Links */}
         <div className="mb-8 text-center md:text-left">
           <h4 className="text-xl font-semibold mb-4">Let's Connect</h4>
